f26abf33b0e9afb3b0c6ba8b970bc3b4380104e7
Joseph Halstead <josephhalstead89@gmail.com>
2016-11-13 00:11:47 +0000

    Added more tests and quotation mark detector

--- piglatin.py
+++ piglatin.py
@@ -16,7 +16,7 @@ def first_vowel_index(word):
 
 	vowel_list = ["a", "e", "i", "o", "u"]
 
-	for index in range(len(word)-1):
+	for index in range(len(word)):
 	
 		if word[index] in vowel_list:
 		
@@ -39,17 +39,25 @@ def pig_latin_word(word):
 
 	vowel_list = ["a", "e", "i", "o", "u"]
 
-	punctuation_list = [".", ",", "?", "!",";", ":"]
+	punctuation_list = [".", ",", "?", "!",";", ":", "'", '"']
 	
 	capital_flag = False
 	punctuation_holder = ""
 	word_length = len(word)
+	quotation_holder = ""
 	
 	if word[word_length-1] in punctuation_list:  #if the last character is a punctuation mark
 	
 		punctuation_holder = word[word_length-1] #store the punctuation mark in a variable
 		
-		word=word[0:word_length-1] #cut the punctuation mark off the end of the word
+		word = word[0:word_length-1] #cut the punctuation mark off the end of the word
+		
+	if word[0] == "'" or word[0] == '"':
+	
+		quotation_holder = word[0]
+		
+		word = word[1:]
+		
 		
 		
 	if word[0].isupper() == True: 
@@ -75,38 +83,49 @@ def pig_latin_word(word):
 			first_letter = first_letter.upper()
 			word = first_letter + word[1:]
 			
-	return word
+	return quotation_holder + word
 
+def pig_latin_sentence(sentence):
 
+	"""
+	Runs the pig_latin_word function on each word in a sentence.
+	
+	Returns a string of the pig latin sentence.
+	
+	Each word in the original is separated by a single space
+	"""
+		
+	sentence = sentence.rstrip() #get rid of newline characters etc.
+		
+	new_string = ""
+
+	word_list = sentence.split()
+	
+
+	for word in word_list:
+
+		new_string = new_string + pig_latin_word(word) + " "
+		
+	return new_string[:len(new_string)-1] #cut off final space
+
+	
 def main():
 
 	input_file = open(sys.argv[1], "r")
 	
 	output_file = open(sys.argv[2], "w")
 	
-	
 	for line in input_file:
 	
-		user_string = line
+		sentence = pig_latin_sentence(line)
 		
-		user_string = user_string.rstrip()
-			
-		new_string = ""
-	
-		word_list = user_string.split()
-	
-		for word in word_list:
-	
-			new_string = new_string + pig_latin_word(word) + " "
-		
-		output_file.write(new_string + "\n")
+		output_file.write(sentence + "\n")
 	
 	
-
 	
 if __name__ == '__main__':
+
 	main()
-	
 	
 	
 	
--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from piglatin import pig_latin_word, first_vowel_index
+from piglatin import pig_latin_word, first_vowel_index, pig_latin_sentence
 
 
 class TestPigLatin(unittest.TestCase):
@@ -13,20 +13,36 @@ class TestPigLatin(unittest.TestCase):
 		self.assertEqual(first_vowel_index('potato') , 1)
 		self.assertEqual(first_vowel_index('banana') , 1)
 		self.assertEqual(first_vowel_index('crime') , 2)
+		self.assertEqual(first_vowel_index('the') , 2)
 		
 		
 	def test_pig_latin_word(self):
 	
-		self.assertEqual(first_vowel_index('trashcan') , 2)
-		self.assertEqual(first_vowel_index('e') , 0)
-		self.assertEqual(first_vowel_index('cry') , False)
-		self.assertEqual(first_vowel_index('Creep') , 2)
-		self.assertEqual(first_vowel_index('potato') , 1)
-		self.assertEqual(first_vowel_index('banana') , 1)
-		self.assertEqual(first_vowel_index('crime') , 2)
+		self.assertEqual(pig_latin_word('pig') , 'igpay')
+		self.assertEqual(pig_latin_word('banana') , 'ananabay')
+		self.assertEqual(pig_latin_word('trash') , 'ashtray')
+		self.assertEqual(pig_latin_word('happy') , 'appyhay')
+		self.assertEqual(pig_latin_word('duck') , 'uckday')
+		self.assertEqual(pig_latin_word('glove') , 'oveglay')
+		self.assertEqual(pig_latin_word('eat') , 'eatway')
+		self.assertEqual(pig_latin_word('are"') , 'areway"')
+		self.assertEqual(pig_latin_word("the'") , "ethay'")
 		
+		self.assertEqual(pig_latin_word('Pig') , 'Igpay')
+		self.assertEqual(pig_latin_word('Banana') , 'Ananabay')
+		self.assertEqual(pig_latin_word('trash!') , 'ashtray!')
+		self.assertEqual(pig_latin_word('happy?') , 'appyhay?')
+		self.assertEqual(pig_latin_word('Duck.') , 'Uckday.')
+		self.assertEqual(pig_latin_word('glove:') , 'oveglay:')
+		self.assertEqual(pig_latin_word('Eat') , 'Eatway')
+		self.assertEqual(pig_latin_word('Are,') , 'Areway,')
+		self.assertEqual(pig_latin_word('Welcome,') , 'Elcomeway,')
 		
+
+	def test_pig_latin_sentence(self):
 		
+		self.assertEqual(pig_latin_sentence('The quick brown fox jumped over the lazy dog!') , 'Ethay uickqay ownbray oxfay umpedjay overway ethay azylay ogday!')
+		self.assertEqual(pig_latin_sentence('And God said, "Let there be light," and there was light.') , 'Andway Odgay aidsay, "Etlay erethay ebay ightlay," andway erethay asway ightlay.')
 		
 		
 		
