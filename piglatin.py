e58ac15336dc20619e99163bdbe05244fec0b30d
Joseph Halstead <josephhalstead89@gmail.com>
2016-11-13 12:30:41 +0000

    added speech marks to punctuation

--- piglatin.py
+++ piglatin.py
@@ -35,7 +35,7 @@ def strip_punctuation(word):
 	Also does the same for numeric characters.
 	"""
 
-	punctuation_list = [".", ",", "?", "!",";", ":", "'", '"', "(", ")", "<", ">", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', "“"]
+	punctuation_list = [".", ",", "?", "!",";", ":", "'", '"', "(", ")", "<", ">", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', "“", "”"]
 
 	punctuation = []
 	
